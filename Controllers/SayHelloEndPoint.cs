56fff86b02c6dac69ef1c35c82a7c70d40e2e9a6
vivartmedia <nanno70@yahoo.de>
2024-01-08 11:51:22 -0800

    Say Hello_Endpoint

---
+++ Controllers/SayHelloEndPoint.cs
@@ -0,0 +1,17 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace NannoA_Say_Hello_Endpoint.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SayHelloEndPoint : ControllerBase
+    {
+        [HttpPost]
+         
+    public string sayHelloApi(string name)
+    {
+        return $"Hello {name}, how are you doing today?";
+    }
+}
+ 
---
+++ README.md
@@ -0,0 +1,6 @@
+Name: Ashur Nanno
+Date: 01/08/2024
+Lab Name - Say Hello - Endpoint
+Description - Created an api endpoint that receives in user name and return output with greeting: Hello, {user's Name}.
+Peer Review by - 
+Peer Review -
